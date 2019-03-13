32cb40bc0ff760fd9175c0791a2e6303721122fa
anwesha1995 <prlanwesha@gmail.com>
2019-03-13 12:28:28 +0530

    Files are added by Anwesha Das

---
+++ Sample.java
@@ -0,0 +1,13 @@
+public class Sample{
+
+	public static void main(String [] args){
+
+		System.out.println("Hello Java World");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("Jenkins java project starting..." +i);
+		}
+
+
+	}
+}
